a336e9fcca8df6aa4c4e779e55fd3d44c584e39a
Antonio J. Polo Enriquez <apoloenriquez@deloitte.es>
2022-10-27 18:25:54 +0200

    hola mundo test screen

--- App.tsx
+++ App.tsx
@@ -1,60 +1,10 @@
 import React from 'react';
 import type {Node} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-
-import {Colors} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import {View} from 'react-native';
+import HolaMundoScreen from './src/screens/HolaMundoScreen';
 
 const App: () => Node = () => {
-  return (
-    <View>
-      <Text>Hola mundo</Text>
-    </View>
-  );
+  return <HolaMundoScreen />;
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
